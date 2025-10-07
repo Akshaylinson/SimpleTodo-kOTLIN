fa428e745af3a920490c9e4366a37829d0e5683a
AKSHAY LINSON M <158825577+Akshaylinson@users.noreply.github.com>
2025-10-07 22:09:09 +0530

    Update AlarmReceiver.kt

--- AlarmReceiver.kt
+++ AlarmReceiver.kt
@@ -1,1 +1,13 @@
+package com.akshay.simpletodo
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val todoText = intent.getStringExtra("todo_text") ?: "Reminder"
+        Toast.makeText(context, "Reminder: $todoText", Toast.LENGTH_LONG).show()
+    }
+}
