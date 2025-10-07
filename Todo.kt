4b89751eb22f27c6530c3e965d17a2c588a8854b
AKSHAY LINSON M <158825577+Akshaylinson@users.noreply.github.com>
2025-10-07 22:07:37 +0530

    Update Todo.kt

--- Todo.kt
+++ Todo.kt
@@ -1,1 +1,13 @@
+package com.akshay.simpletodo
+
+import java.util.UUID
+
+data class Todo(
+    val id: String = UUID.randomUUID().toString(),   // Use UUID as unique string ID
+    var text: String,
+    var done: Boolean = false,
+    var priority: Int = 0,
+    var starred: Boolean = false,
+    var dueMillis: Long? = null
+)
 
