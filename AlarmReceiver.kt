faaeec505314e6b0f405e9e228c6fb643386f7b0
AKSHAY LINSON M <158825577+Akshaylinson@users.noreply.github.com>
2025-10-08 23:21:37 +0530

    Update AlarmReceiver.kt

--- AlarmReceiver.kt
+++ AlarmReceiver.kt
@@ -1,9 +1,4 @@
-package com.akshay.simpletodo
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.widget.Toast
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
